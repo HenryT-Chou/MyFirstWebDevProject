3d270adabf5d64c223e567b5c433b65836706d05
Henry Chou <henry.chou714@gmail.com>
2020-06-30 16:27:44 -0700

    finished the youtube crash course

--- Drone_Website/app.js
+++ Drone_Website/app.js
@@ -1,17 +1,25 @@
 
 //create a variable called pics, and store the 2 images that I have //for the button to cycle through
 var pics = [
-  "pictures/horizontal.png",
+  "pictures/horizontal .jpg",
   "pictures/assembly.png"
 ]
+
+var imgs = document.querySelector("img");
+var counter = 0;
 
 //again use the # to identify id's
 var realBtn = document.querySelector("#realButton");
 // what ever is in the curly braces will activate when button is clicked"
 realBtn.addEventListener("click", function(){
   // a function called alert that creates a pop up notification in the broswer
-  alert("CONGRATS!!! you clicked the real button!")
+  if(counter === 2){
+    counter = 0;
+  }
+  imgs.src = pics[counter]
+  counter = counter + 1;
 })
+
 
 var fakeBtn = document.querySelector("#fakeButton");
 fakeBtn.addEventListener("click", function(){
